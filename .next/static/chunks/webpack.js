20ce1ec6fd406b0036f07c0ea073d95cc9003fc8
aleksname <aleksname.v2@gmail.com>
2024-05-24 11:50:28 +0300

    change: good refresh Random page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de6a778cbfef02f6"; }
+/******/ 		__webpack_require__.h = function() { return "fc09a8fed643fd20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Random.jsx
+++ pages/Random.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from 'react';
 import MainContainter from './components/MainContainter';
-import Loading from './components/Loading'; // імпорт компонента Loading
 import styles from '../styles/App.module.scss';
 import Link from 'next/link';
 
 export default function Random() {
-    const [isLoading, setIsLoading] = useState(true); // стан для контролю анімації завантаження
     const [randomData, setRandomData] = useState([]);
-
     const data = ['Єгор', 'Софія', 'Аміна', 'Артем', 'Алан та Вєлат'];
-
     const shuffleArray = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -17,31 +13,28 @@ export default function Random() {
         }
         return array;
     };
-
     useEffect(() => {
         const shuffledData = shuffleArray([...data]);
         setRandomData(shuffledData.slice(0, 6)); // Вибрати перші 6 елементів після перемішування
-
-        const timer = setTimeout(() => {
-            setIsLoading(false); // вимкнути анімацію після 4-5 секунд
-        }, 4000);
-
-        return () => clearTimeout(timer); // очистити таймер при демонтажі компонента
     }, []);
-
-    if (isLoading) {
-        return <Loading />; // показати анімацію завантаження, поки isLoading true
-    }
 
     return (
         <MainContainter titels={'Random page'}>
             <div className={styles.randomBlock}>
-                <nav className={styles.navRandomPages}>
+                {/* <nav className={styles.navRandomPages}>
                     <Link href='Random' className={styles.linkRandom}>Random color</Link>
                     <Link href='Spin' className={styles.linkRandom}>Spin</Link>
-                </nav>
+                </nav> */}
 
                 <div className={styles.randomBlockArea}>
+                        <strong className={styles.randomTitle}>Я - старий чаклун із далекої чарівної країни. Сьогодні до мене долетів магічний сигнал, і він розповів мені дещо неймовірне про кожного з вас. Кожен із вас має свій особливий колір, який супроводжує вас у житті та надає вам сили та натхнення. Цей колір - частина вашої природи, вашого внутрішнього світу.
+                        
+                        <p  className={styles.randomTitleSpan}>
+                             Давайте разом дізнаємося, який колір належить саме вам! Приготуйтеся до магічної подорожі, де колесо фортуни розкриє ваш колір долі та порядок здачі ваших робіт. Чарівництво починається!
+                        </p>
+                    </strong>
+                    <strong className={styles.randomTitle2}>Ось ваші кольори &#10552;</strong>
+                    <p className={styles.randomTitle}></p>
                     <ul className={styles.randomUl}>
                         {randomData.map((item, index) => (
                             <li key={index} className={styles.randomLi}>
