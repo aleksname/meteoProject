a8a60aecd40a35dff74d68feeb2d6c3c2c045c7d
aleksname <aleksname.v2@gmail.com>
2024-05-31 13:19:47 +0300

    change: refrech json file? for deploy on the Vercel

--- pages/components/SpinSlider/SpinSlider.jsx
+++ pages/components/SpinSlider/SpinSlider.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import styles from '../../../styles/App.module.scss';
-// import required modules
 import { Pagination, FreeMode, EffectCoverflow } from 'swiper/modules';
 import SpinSlids from './SpinSlids';
-
 
 import icon1 from '../img/Random/RedRune.png';
 import icon2 from '../img/Random/BlueRune.png';
@@ -19,45 +15,81 @@ import icon3 from '../img/Random/PurpleRune.png';
 import icon4 from '../img/Random/BrownRune.png';
 import icon5 from '../img/Random/GreenRune.png';
 import icon6 from '../img/Random/QuestionPune.png';
-import Link from 'next/link';
+
+
+import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
+import { useState } from 'react';
 
 export default function SpinSlider() {
+
+  const [anchor, setAnchor] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchor(anchor ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchor);
+  const id = open ? 'simple-popup' : undefined;
   
   return (
     <>
       <p className={styles.swipersSliderTitle}>
          КХ - КХ, ось і міфічні символи, які ми маємо прямо зараз розгадати
       </p>
-      <Swiper 
-      className={styles.mySwiper}
-      effect={'coverflow'}
-      grabCursor={true}
-      coverflowEffect={{
-          rotate: 40,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-        }}
-      slidesPerView={2.6}
-      centeredSlides={true}
-      // loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[FreeMode, Pagination, EffectCoverflow]}
-      spaceBetween={200}
+       
+      <BasePopup
+        id={id}
+        open={open}
+        anchor={anchor}
+        disablePortal
+        className="z-50 rounded-lg font-sans font-medium text-sm mt-2 p-3 border border-solid border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 shadow-md text-slate-900 dark:text-slate-100"
       >
-        <button>
-        <Link href='/'>
-          <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon1} className={styles.spinSlids} /></SwiperSlide>
-        </Link>        
-        </button>
-        <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon2} className={styles.spinSlids}/></SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon3} className={styles.spinSlids}/></SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon4} className={styles.spinSlids} /></SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon5} className={styles.spinSlids} /></SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}><SpinSlids src={icon6} className={styles.spinSlids} /></SwiperSlide>
-
+    <div>el</div>
+      </BasePopup>
+      <Swiper 
+        className={styles.mySwiper}
+        effect={'coverflow'}
+        grabCursor={true}
+        coverflowEffect={{
+            rotate: 40,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+          }}
+        slidesPerView={2.6}
+        centeredSlides={true}
+        // loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination, EffectCoverflow]}
+        spaceBetween={200}
+      >
+        <SwiperSlide className={styles.swiperSlide}>
+          <button
+            aria-describedby={id}
+            type="button"
+            onClick={handleClick}
+          >
+            <SpinSlids src={icon1} className={styles.spinSlids} />
+          </button>
+          
+        </SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
+          <SpinSlids src={icon2} className={styles.spinSlids}/>
+        </SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide} >
+          <SpinSlids src={icon3} className={styles.spinSlids}/>
+        </SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide} >
+          <SpinSlids src={icon4} className={styles.spinSlids} />
+        </SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide} >
+          <SpinSlids src={icon5} className={styles.spinSlids} />
+        </SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide} >
+          <SpinSlids src={icon6} className={styles.spinSlids} />
+        </SwiperSlide>
       </Swiper>
     </>
   );
